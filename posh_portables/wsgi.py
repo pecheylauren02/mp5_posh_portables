6076992a8877b733fcd232fe83e409e4c0fc9f98
pecheylauren02 <pechey.lauren02@gmail.com>
2023-11-28 10:43:40 +0000

    initial commit

---
+++ posh_portables/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for posh_portables project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'posh_portables.settings')
+
+application = get_wsgi_application()
