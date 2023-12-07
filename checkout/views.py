672af6f9794a88e0f2f4ecb4efc47cf2e91908e7
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-07 09:42:11 +0000

    add js functionality for stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OKdbyHZ7jyXnuyb2YonXHz3B95UiQ0G17U8zvGUWOOL3hkCWrT5hbTgZRLyQTfQoRpGQGx0taaMkPEcfVNhFsaP000Pm6yWxH',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
