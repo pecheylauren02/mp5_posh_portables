0692fb002005ab44f94985471f9ba05fdfb5923e
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-07 13:13:43 +0000

    add debug for development

--- posh_portables/settings.py
+++ posh_portables/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-pecheylaure-mp5poshport-xt9xerlm3wd.ws-eu106.gitpod.io', 'posh-portables.herokuapp.com', 'localhost', 'posh-portables-6801c9648247.herokuapp.com',]
 
