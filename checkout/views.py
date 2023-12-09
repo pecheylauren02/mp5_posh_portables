d6b91e6bf92626a39d23e1944d55f39eee99f77c
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 11:32:06 +0000

    add stripe intent for public key

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,11 @@ from shopping_cart.contexts import shopping_cart_contents
 import stripe 
 
 def checkout(request):
+    """ Connects the form with Stripe to process all payments """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     shopping_cart = request.session.get('shopping_cart', {})
 
     if not shopping_cart:
@@ -16,12 +21,22 @@ def checkout(request):
     current_shopping_cart = shopping_cart_contents(request)
     total = current_shopping_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Uh oh! Your Stripe public key is missing. \
+                         Did you forget to set it in your environment?')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OKdbyHZ7jyXnuyb2YonXHz3B95UiQ0G17U8zvGUWOOL3hkCWrT5hbTgZRLyQTfQoRpGQGx0taaMkPEcfVNhFsaP000Pm6yWxH',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
