74cbcb5599095fb36f4f5d122817c25ae4bd303f
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 15:38:16 +0000

    add webhook handlers for success and fail

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -8,8 +8,26 @@ class Stripe_Webhook_Handler:
         self.request = request
 
     def handle_event(self, event):
-        """ Handles generic, unknown or unexpected webhook events """
+        """ 
+        Handles generic, unknown or unexpected webhook events 
+        """
 
+        return HttpResponse(
+            content=f'Your Webhook has been received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handles successful payment intent webhooks from Stripe
+        """
+        return HttpResponse(
+            content=f'Your Webhook has been received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handles failed payment intent webhooks from Stripe
+        """
         return HttpResponse(
             content=f'Your Webhook has been received: {event["type"]}',
             status=200)
---
+++ checkout/webhooks.py
@@ -0,0 +1,8 @@
+
+
+def webhook(request):
+    """
+    Listens for webhooks from Stripe
+    """
+
+    
