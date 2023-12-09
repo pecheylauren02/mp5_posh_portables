f5879a5dcf3849754901d526bfb86975c76038f4
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 12:15:36 +0000

    add form to checkout to process payment

--- checkout/views.py
+++ checkout/views.py
@@ -6,32 +6,51 @@ from shopping_cart.contexts import shopping_cart_contents
 
 import stripe 
 
+
 def checkout(request):
     """ Connects the form with Stripe to process all payments """
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    shopping_cart = request.session.get('shopping_cart', {})
+    if request.method == 'POST':
+        shopping_cart = request.session.get('shopping_cart', {})
 
-    if not shopping_cart:
-        messages.error(request, "Your shopping cart is empty at the moment!")
-        return redirect(reverse('products'))
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone': request.POST['phone'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'city': request.POST['city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+    else:
 
-    current_shopping_cart = shopping_cart_contents(request)
-    total = current_shopping_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        shopping_cart = request.session.get('shopping_cart', {})
+        if not bag:
+            messages.error(request, "There's nothing in your shopping cart at the moment")
+            return redirect(reverse('products'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request, 'Uh oh! Your Stripe public key is missing. \
-                         Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
-    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
