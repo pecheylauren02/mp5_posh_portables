b1d2894f93c8fc4471f0d125571ab2f179c8eb7c
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 18:19:09 +0000

    update models with pip

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,13 +11,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date', 'delivery_cost',
-                        'order_total', 'grand_total',)
+                        'order_total', 'grand_total',
+                        'original_shopping_cart', 'stripe_pid')
     
     fields = (('order_number', 'date', 'first_name',
                'last_name', 'email', 'phone', 'country', 'postcode', 
                'city', 'street_address1', 'street_address2',
                'county', 'delivery_cost',
-               'order_total', 'grand_total',))
+               'order_total', 'grand_total',
+               'original_shopping_cart', 'stripe_pid'))
 
     list_display = ('order_number', 'date', 'first_name',
                     'last_name', 'order_total', 'grand_total',)
