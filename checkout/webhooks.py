e65832af24f0d2d6478e1cbead6568cc7044cc1e
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 15:56:14 +0000

    add webhook handlers and keys to env.py

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,9 +1,23 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
+from checkout.webhook_handler import Stripe_Webhook_Handler
 
+import stripe
+
+@require_POST
+@csrf_exempt
 def webhook(request):
     """
     Listens for webhooks from Stripe
     """
+
+    # Setup 
+    webhook_secret = settings.STRIPE_WEBHOOK_SECRETKEY
+    stripe.api.key = settings.STRIPE_SECRET_KEY
+
     # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
@@ -31,4 +45,16 @@ def webhook(request):
         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
-    
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
+
+    print('Successful!')
+    return HttpResponse(status=200)
