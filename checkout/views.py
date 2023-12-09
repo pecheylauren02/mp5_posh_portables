432a2dd988d5415f852b2a757dce264d900f5f9b
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-09 16:19:55 +0000

    add function to handle all cache checkout data

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -9,6 +10,26 @@ from products.models import Product
 from shopping_cart.contexts import shopping_cart_contents
 
 import stripe 
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Handles all cache checkout data
+    """
+    try:
+        pid = request.POST.get('client_secret').split('secret')[0]
+        stripe.api.key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'shopping_cart': json.dumps(request.session.get('shopping_cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
