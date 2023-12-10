41b77a56390c829f61997481517c9e45713ad1ae
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-10 14:25:01 +0000

    update profile views and checkout success button

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,7 @@ from .forms import UserProfileForm
 
 
 def profile(request):
-    """ Display the user's profile. """
+    """ Displays the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -27,4 +27,20 @@ def profile(request):
 
     return render(request, template, context)
 
-def order_history
+def order_history(request, order_number):
+    """ Displays the user's order history """
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
