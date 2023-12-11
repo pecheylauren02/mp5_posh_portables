a379984aeb355bc9f74fb3a0e243c12039a0fdb5
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-11 11:51:47 +0000

    add delete functionality and templates

--- products/views.py
+++ products/views.py
@@ -81,9 +81,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'You have successfully added your product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure you have filled out the form correctly.')
     else:
@@ -122,3 +122,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ 
+    Store owner can delete a product from the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Your Product has been deleted!')
+    return redirect(reverse('products'))
