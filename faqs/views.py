065a8038cf7d1bf5a68ca4e1c98c66b1043044d2
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-15 13:36:38 +0000

    add function to create faq in view

--- faqs/views.py
+++ faqs/views.py
@@ -18,3 +18,44 @@ def faqs(request):
     }
 
     return render(request, 'faqs/faqs.html', context)
+
+
+@login_required()
+def create_faq(request):
+    """
+    Renders form to create FAQ.
+    Admin only can access this.
+    Adds new faq to database.
+    """
+
+    # Checks user is admin
+    # redirects to faqs if not
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only administrators can create FAQs.')
+        return redirect(reverse('faqs'))
+
+    # Handles Form Submission
+    if request.method == "POST":
+        form = FaqForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "New FAQ added.")
+            return redirect(reverse('faqs'))
+        else:
+            messages.error(request, "Form invalid, please try again.")
+
+    # Handles View Rendering
+    else:
+        form = FaqForm()
+
+    # Sets page template
+    template = 'faqs/add_faq.html'
+
+    # Sets current faq & form content
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
