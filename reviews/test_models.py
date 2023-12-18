5f8152ec0f5b3b2d126c09dcb6d5a392f2b985a4
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-18 11:43:48 +0000

    fix reviews models tests and html tags

--- reviews/test_models.py
+++ reviews/test_models.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from datetime import date
 from django.utils import timezone
+from freezegun import freeze_time
 
 from products.models import Product, Category
 from django.contrib.auth.models import User
 from reviews.models import Reviews
 
 
+@freeze_time("2022-08-15")
 class TestReviewModels(TestCase):
     """
     Reviews Model Tests
@@ -18,7 +20,7 @@ class TestReviewModels(TestCase):
         """
 
         self.categoryTest = Category.objects.create(
-            name="keyboards",
+            name="Keyboards",
             friendly_name="keyboards",
         )
 
@@ -26,8 +28,7 @@ class TestReviewModels(TestCase):
             category=self.categoryTest,
             name="Gaming Keyboard",
             description="Test description for Gaming Keyboard",
-            price=29.99,
-            is_featured=True,
+            price=53.99,
         )
 
         self.userTest = User.objects.create(
@@ -36,7 +37,7 @@ class TestReviewModels(TestCase):
             email='testuseremail@email.com',
         )
 
-        self.reviewTest = Review.objects.create(
+        self.reviewTest = Reviews.objects.create(
             product=self.productTest,
             user=self.userTest,
             title="test review title",
@@ -44,3 +45,32 @@ class TestReviewModels(TestCase):
             rating=3,
             created_on=timezone.now(),
         )
+
+    def test_review_string_method(self):
+        """ Tests the string method on the review model """
+        review = Reviews(title='test review title', user=self.userTest, product=self.productTest)
+        self.assertEqual(str(review), 'test review title by user1 for Gaming Keyboard')
+
+    def test_review_title(self):
+        """ Test the review title """
+        self.assertEqual(self.reviewTest.title, 'test review title')
+
+    def test_review_content(self):
+        """ Test the review content """
+        self.assertEqual(self.reviewTest.content, 'test review content')
+
+    def test_review_rating(self):
+        """ Test the review rating """
+        self.assertEqual(self.reviewTest.rating, 3)
+
+    def test_review_created_on(self):
+        """ Test the review content """
+        self.assertEqual(self.reviewTest.created_on, date(2022, 8, 15))
+
+    def test_review_product_name(self):
+        """ Test the review product """
+        self.assertEqual(self.reviewTest.product.name, 'Gaming Keyboard')
+
+    def test_review_product_category(self):
+        """ Test the review product category """
+        self.assertEqual(self.reviewTest.product.category.name, 'Keyboards')
--- reviews/test_views.py
+++ reviews/test_views.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from django.utils import timezone
+from freezegun import freeze_time
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from products.models import Product, Category
+from reviews.models import Reviews
+
+
+@freeze_time("2022-08-15")
+class TestReviewsViews(TestCase):
+    """
+    Review Views Tests
+    """
+
+    def setUp(self):
+        """
+        Creates test objects for Reviews app
+        """
+
+        userTest = User.objects.create_user(
+            username="user1",
+            password="password1",
+            email='testuseremail@email.com',
+        )
+        userTest.save()
+
+        superuserTest = User.objects.create_superuser(
+            username="superuser1",
+            password="super_password1",
+            email='testsuperuseremail@email.com',
+        )
+        superuserTest.save()
+
+        self.categoryTest = Category.objects.create(
+            name="Keyboards",
+            friendly_name="keyboards",
+        )
+
+        self.productTest = Product.objects.create(
+            category=self.categoryTest,
+            name="Gaming Keyboard",
+            description="Test description for Gaming Keyboard",
+            price=53.99,
+            is_featured=True,
+            image='image-file'
+        )
+
+        self.reviewTest = Reviews.objects.create(
+            product=self.productTest,
+            user=userTest,
+            title="test review title",
+            content="test review content",
+            rating=3,
+            created_on=timezone.now(),
+        )
+
+        self.reviewTest2 = Reviews.objects.create(
+            product=self.productTest,
+            user=superuserTest,
+            title="test review title 2",
+            content="test review content 2",
+            rating=5,
+            created_on=timezone.now(),
+        )
