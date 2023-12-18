7200746ccf57d70ffa3fe27601ef94010c5cbeb2
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-18 13:01:38 +0000

    test: add update reviews testing

--- reviews/test_views.py
+++ reviews/test_views.py
@@ -84,3 +84,55 @@ class TestReviewsViews(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(
             response, '/accounts/login/?next=%2Freviews%2Fcreate_reviews%2F1')
+
+    def test_update_reviews_page_for_authorized_user(self):
+        """ Test update_reviews view for logged in user"""
+
+        logged_in = self.client.login(
+            username='user1', password='password1')
+        self.assertTrue(logged_in)
+
+        response = self.client.get('/reviews/update_reviews/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reviews/update_reviews.html')
+
+    def test_update_reviews_page_for_logged_out_user(self):
+        """
+        Checks update_reviews redirects to login
+        if user is not logged in
+        """
+
+        response = self.client.get('/reviews/update_reviews/1/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '/accounts/login/?next=%2Freviews%2Fupdate_reviews%2F1%2F')
+
+    def test_update_reviews_page_for_unauthorized_user(self):
+        """
+        Checks update_reviews redirects to product_details
+        if user is not author of review
+        """
+
+        logged_in = self.client.login(
+            username='user1', password='password1')
+        self.assertTrue(logged_in)
+
+        response = self.client.get('/reviews/update_reviews/2/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '/products/1')
+
+    def test_update_other_user_review_page_for_superuser(self):
+        """
+        Checks update_reviews redirects to product_details
+        if superuser is not author of review
+        """
+
+        logged_in = self.client.login(
+            username='superuser1', password='super_password1')
+        self.assertTrue(logged_in)
+
+        response = self.client.get('/reviews/update_reviews/1/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '/products/1')
