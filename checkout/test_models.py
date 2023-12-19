e3be9998dc461a3805d2bb891ae5cf71c1a08682
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-19 11:48:22 +0000

    test: add testcases for products and delivery

--- checkout/test_models.py
+++ checkout/test_models.py
@@ -105,3 +105,57 @@ class TestCheckoutModels(TestCase):
     def test_order_phone(self):
         """ Test the order phone """
         self.assertEqual(self.orderTest.phone, '0123456789')
+    
+    def test_order_address(self):
+        """ Test the order address """
+        self.assertEqual(self.orderTest.street_address1, 'Test Address 1')
+        self.assertEqual(self.orderTest.street_address2, 'Test Address 2')
+        self.assertEqual(self.orderTest.city, 'Test City')
+        self.assertEqual(self.orderTest.county, 'Test County')
+        self.assertEqual(self.orderTest.postcode, 'AA11 2BB')
+        self.assertEqual(self.orderTest.country, 'GB')
+
+    def test_order_total(self):
+        """ Test the order total on the order model """
+        self.assertEqual(self.orderTest.order_total, (
+            self.orderLineItemTest1.lineitem_total +
+            self.orderLineItemTest2.lineitem_total +
+            self.orderLineItemTest3.lineitem_total +
+            self.orderLineItemTest4.lineitem_total
+            )
+        )
+
+    def test_delivery_cost(self):
+        """
+        Tests the delivery cost on the order model
+        Ensure doesn't include the products with no delivery charge
+        """
+        self.assertEqual(self.orderTest.delivery_cost, 0)
+
+    def test_grand_total(self):
+        """ Tests the grand total on the order model """
+        self.assertEqual(
+            self.orderTest.grand_total,
+            self.orderTest.order_total + self.orderTest.delivery_cost)
+
+    def test_order_line_item_order(self):
+        """ Tests the order line item has the correct total """
+        self.assertEqual(
+            self.orderLineItemTest1.order.order_number, '1234567890')
+
+    def test_order_line_item_product(self):
+        """ Tests the order line item has the correct total """
+        self.assertEqual(self.orderLineItemTest1.product.name, "Laptop")
+
+    def test_order_line_item_quantity(self):
+        """ Tests the order line item has the correct total """
+        self.assertEqual(self.orderLineItemTest1.quantity, 1)
+
+    def test_order_line_item_total(self):
+        """ Tests the order line item has the correct total """
+        self.assertEqual(
+            self.orderLineItemTest3.lineitem_total, (
+                self.orderLineItemTest3.product.price *
+                self.orderLineItemTest3.quantity)
+        )
+        self.assertEqual(self.orderLineItemTest3.lineitem_total, 800)
