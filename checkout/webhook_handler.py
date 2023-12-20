d4dce5db65e7b6789ec34c147a8efc16f9eba2cf
pecheylauren02 <pechey.lauren02@gmail.com>
2023-12-20 08:56:40 +0000

    fix: remove repeated code for webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
-from .models import Order, OrderLineItem
-from products.models import Product
-from profiles.models import UserProfile
-
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
+from .models import Order, OrderLineItem
+from products.models import Product
+from profiles.models import UserProfile
+
 import json
 import time
-import stripe
 
 
 class Stripe_Webhook_Handler:
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -41,8 +41,7 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed':
-        handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
@@ -50,29 +49,6 @@ def webhook(request):
 
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
-    event_handler = event_map.get(event_type, handler.handle_event)
-
-    # Call the event handler with the event
-    response = event_handler(event)
-    return response
-
-    print('Successful!')
-    return HttpResponse(status=200)
-
-    # Setup a webhook handler
-    handler = Stripe_Webhook_Handler(request)
-
-    # Map all of the webhook events to relevant handler functions
-    event_map = {
-        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed':
-        handler.handle_payment_intent_payment_failed,
-    }
-
-    # Get the webhook type from Stripe
-    event_type = event['type']
-
-    # Generic default handler will be used by default
     event_handler = event_map.get(event_type, handler.handle_event)
 
     # Call the event handler with the event
